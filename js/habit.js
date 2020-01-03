cb76f62903c72d99b065df065515a5095b531b78
Chinmay Shah <chinmayshah3899@gmail.com>
2020-01-03 16:04:49 +0530

    improved habit

---
+++ js/habit.js
@@ -0,0 +1,57 @@
+//Define Angular app
+
+var app = angular.module("TodoListApp", []);
+
+//Define controller
+x = []
+
+app.controller("todoListController", ['$scope', function($scope) {
+
+    $scope.todos = []
+    $scope.addTodo = function(todo) {
+        if (todo != '' || typeof todo == undefined) {
+	        $scope.todos.push(todo);
+	        $scope.newItem = '';
+	    }
+        else { return false; }
+        
+    console.log($scope.todos)
+    x = $scope.todos
+    }
+}]);
+
+
+function myfunc(){
+    console.log('hello, world')
+    console.log(x)
+    if(document.getElementById("emailer").value === ''){
+
+    }
+    else{      
+        const tz = Date()
+        console.log(tz);
+        var url = 'http://localhost:8080/api/addGoals'
+        var request = new XMLHttpRequest();
+        var parm = {
+            email: document.getElementById("emailer").value,
+            time: document.getElementById('timer').value,
+            todo: (x),
+            date: Date()
+        }
+        var params = JSON.stringify(parm)
+        request.open('POST', url, true);
+        // request.onreadystatechange = function() {if (request.readyState==4) alert("It worked!");};
+        request.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+                if (this.responseText === 'hello'){
+                    window.location.href = "final.html";
+                }
+            }
+        };
+        request.setRequestHeader("Content-type", "application/json");
+        request.setRequestHeader("Content-length", params.length);
+        request.setRequestHeader("Connection", "close");
+        request.send(params);
+    }
+}
